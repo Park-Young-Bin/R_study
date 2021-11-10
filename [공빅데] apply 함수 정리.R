f8cac7ec469cf89eec31957fc4fe73f2b52ed62d
Park-Young-Bin <78736920+Park-Young-Bin@users.noreply.github.com>
2021-11-10 20:18:45 +0900

    upload

--- [공빅데] apply 함수 정리.R
+++ [공빅데] apply 함수 정리.R
@@ -29,3 +29,21 @@ apply(Titanic, 1, sum)
 apply(Titanic, c(1, 4), sum)
 
 # lapply----
+# 리스트
+trees
+
+typeof(trees)
+
+lapply(trees, mean)
+lapply(trees, class)
+
+# sapply----
+# sapply:lapply 함수에서 이용에 편리함을 부여한 함수,벡터 형태로 단순화해서 결과 출력
+sapply(trees, mean, simplify = T)
+
+# vapply----
+vapply(trees, mean, numeric(1))
+
+vapply(trees, mean) # 에러
+
+vapply(trees, fivenum, c(Min. = 0, 'lst Qu.' = 0, Median = 0, '3rd Qu.' = 0, Max. = 0))
